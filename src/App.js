706fc522fcf5f28c0a64ee953e7e5fce34761e68
Eldiyar Talantbek <eldiyartalantbek@Eldiyars-MacBook-Pro.local>
2019-06-03 17:32:35 -0500

    converting App to class component

--- src/App.js
+++ src/App.js
@@ -6,25 +6,27 @@ const Pet = (props) => {
     ]);
 }
 
-const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, 'Adopt me!'),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "dog",
-            breed: "Havanese"
-        }),
-        React.createElement(Pet, {
-            name: "Pepper",
-            animal: "bird",
-            breed: "Cockatiel"
-        }),
-        React.createElement(Pet, {
-            name: "Kiara",
-            animal: "dog",
-            breed: "Mixed"
-        })
-    ]);
-};
+class App extends React.Component {
+    render() {
+        return React.createElement("div", {}, [
+            React.createElement("h1", {}, 'Adopt me!'),
+            React.createElement(Pet, {
+                name: "Luna",
+                animal: "dog",
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                name: "Pepper",
+                animal: "bird",
+                breed: "Cockatiel"
+            }),
+            React.createElement(Pet, {
+                name: "Kiara",
+                animal: "dog",
+                breed: "Mixed"
+            })
+        ]);
+    }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
